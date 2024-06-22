5d02c66cb70867658415c5e334da84cc581b18cb
ncsreejith@gmail.com <ncsreejith@gmail.com>
2024-06-21 22:46:21 -0400

    Pushing code for first time

---
+++ src/main/java/TestSuites/firstTestcase.java
@@ -0,0 +1,26 @@
+package TestSuites;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class firstTestcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.progressive.com/");
+		 String url_store = driver.getCurrentUrl();
+		String actual_title = driver.getTitle();
+		System.out.println("The Title is:");
+		System.out.println(actual_title);
+		System.out.println(url_store);
+		System.out.println("Tests are Passed");
+		driver.manage().window().maximize();
+		//driver.close();
+		
+				
+
+	}
+
+}
